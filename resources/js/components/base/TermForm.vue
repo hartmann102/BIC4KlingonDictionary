202299ad01a821327e139b5a0d6d2530aea6f6a5
hartmann102 <ic19b102@technikum-wien.at>
2021-06-04 13:39:14 +0200

    added file

---
+++ resources/js/components/base/TermForm.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="TermForm">
+    <query-message :success="this.form.isSuccess()" :fail="this.form.isFail()" :message="form.isSuccess() ? 'Success' : form.failMessage"></query-message>
+    <form @submit.prevent = "submit">
+      <label>name</label>
+      <input class="input" type="text" v-model="form.name">
+      </input>
+      <label>desription</label>
+      <input class="input" type="text" v-model="form.description">
+      </input>
+
+<button class="button" type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+
+
+    let form = new Form({
+        'translation_id': '',
+        'name': '',
+        'description': '',
+        'term_id': '',
+    });
+
+    export default {
+        name: "TermForm",
+        data() {
+            return {
+                form: form,
+                terms : [],
+                }
+          },
+          created () {
+          axios.get('/list/term')
+              .then(response => {
+                  this.terms = response.data;
+                  })
+          },
+          components : {
+            QueryMessage
+          },
+          methods : {
+          submit () {
+            this.form.post("/term");
+          }
+          }
+}
+
+
+
+</script>
